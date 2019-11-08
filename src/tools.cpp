f9aef4dfda9353f3786ea8eaadc1c2237cdc75df
rkopec91 <ryankopec91@gmail.com>
2019-11-08 00:22:27 -0500

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,14 +11,38 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  for(unsigned int i=0; i < estimations.size(); ++i){
+    VectorXd residuals = estimations[i] - ground_truth[i];
+    residuals = residuals.array() * residuals.array();
+    rmse += residuals;
+  }
+
+  rmse = rmse / estimations.size();
+
+  rmse = sqrt(rmse.array());
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+  MatrixXd Hj(3,4);
+
+  float pos_x = x_state(0);
+  float pos_y = x_state(1);
+  float vel_x = x_state(2);
+  float vel_y = x_state(3);
+
+  float h1 = pos_x * pos_x + pos_y * pos_y;
+
+  float h2 = sqrt(h1);
+  float h3 = h1 * h2;
+
+  Hj << pos_x/h2, pos_y/h2, 0, 0,
+        -pos_y/h1, pos_x/h1, 0, 0,
+        (pos_y*(vel_x*pos_y - vel_y*pos_x))/h3, (pos_x*(vel_y*pos_x - vel_x*pos_y))/h3, pos_x/h2, pos_y/h2;
+
+  return Hj;
 }
